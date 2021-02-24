737286e40c9fea7158a581a2c51776ee39b5d2ca
Anthony Mensah <mensahanthony356@gmail.com>
2021-02-24 16:27:56 +0000

    Menu Model Updates

---
+++ Lunch App/Models/Menu/EditMenu.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lunch_App.Models.Menu
+{
+    public class EditMenu
+    {
+
+        public Guid Id { get; set; }
+
+
+        public DateTime StartAt { get; set; }
+
+        public Guid MainDishId { get; set; }
+
+        public Guid SideDishId { get; set; }
+
+        public string Price { get; set; }
+
+        public Guid CondiDishId { get; set; }
+
+        public DateTime EndAt { get; set; }
+
+    }
+}
+
